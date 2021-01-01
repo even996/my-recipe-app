8d7725fdb52bc83100a6557f4f9d82863a5d03d1
Even Olsen <evenolsen96@gmail.com>
2021-01-01 14:07:01 +0100

    Fiksa noen små ting: farger, størrelser

--- src/Recipe.js
+++ src/Recipe.js
@@ -4,20 +4,22 @@ import style from './recipe.module.css';
 // bare for å få vekk en error med elementene for være unike for å letter fjerne de i react
 let i = 0;
 
-const Recipe = ({title, calories, image, ingredients}) => {
-    return(
-        <div className={style.recipe}>
-            <h1>{title}</h1>
-            <ol>
-                {ingredients.map(ingredient => (
-                    <li key = {i = i+1}>{ingredient.text}</li>
-                ))}
-            </ol>
-            <p>{calories}</p>
-            <img className={style.image} src={image} alt=""/>
-        </div>
-    )
-}
+const Recipe = ({ title, calories, image, ingredients }) => {
+  const calNum = parseFloat(calories);
+  const cal = calNum.toFixed(1);
+  const calString = cal.toString();
+  return (
+    <div className={style.recipe}>
+      <h2>{title}</h2>
+      <ol>
+        {ingredients.map((ingredient) => (
+          <li key={(i = i + 1)}>{ingredient.text}</li>
+        ))}
+      </ol>
+      <p>{calString} calories</p>
+      <img className={style.image} src={image} alt='' />
+    </div>
+  );
+};
 
 export default Recipe;
-
